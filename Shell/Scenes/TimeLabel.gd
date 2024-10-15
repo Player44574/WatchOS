c058645fc007fe23e9766225ccdf7cf28d8424a7
Player44574 <67709545+Player44574@users.noreply.github.com>
2024-10-16 00:15:20 +0200

    main screen

--- Shell/Scenes/TimeLabel.gd
+++ Shell/Scenes/TimeLabel.gd
@@ -14,7 +14,15 @@ func update_time():
 	var real_hour = Time.get_datetime_dict_from_system().hour
 	var real_min = Time.get_datetime_dict_from_system().minute
 	
+	var real_day = Time.get_datetime_dict_from_system().day
+	var real_month = Time.get_datetime_dict_from_system().month
+	var real_year = Time.get_datetime_dict_from_system().year
+	
 	var hours = str(real_hour).pad_zeros(2)
 	var minutes = str(real_min).pad_zeros(2)
 	
-	text = "[center]" + hours + ":" + minutes + "[/center]"
+	var day = str(real_day).pad_zeros(2)
+	var month = str(real_month).pad_zeros(2)
+	var year = str(real_year)
+	
+	text = "[right]" + hours + ":" + minutes + "[/right]" + "[right]" + day + "/" + month + "/" + year + "[/right]"
